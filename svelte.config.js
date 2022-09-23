c14edaebf385b4257faa4a5eddbc6afda43bf5cd
E_San <ls2wist@gmail.com>
2022-09-23 12:42:08 +0900

    add

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: adapter()
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
