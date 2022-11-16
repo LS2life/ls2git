7d48438d4ffac256bcf044895539ceb1840fbe19
lss <ls2wist@gmail.com>
2022-11-17 03:10:11 +0900

    동적 홈페이지를 만들어보자

--- src/routes/lab/infotech/framework/svelte/+page.svelte
+++ src/routes/lab/infotech/framework/svelte/+page.svelte
@@ -1,5 +1,8 @@
 <h1>Svelte</h1>
-<h1>Svelte</h1>
-<h1>Svelte</h1>
-<h1>Svelte</h1>
-<h1>Svelte</h1>
+ - Svelte로 Dynamic web page를 만들어보자
+
+<h2>Sveltekit</h2>
+ - sveltekit도 동적페이지를 만들수 있다.  
+ - kit이라는 말은 자유도가 조금은 떨어질 수 있다는 느낌.  
+
+<h1>svelte로 한 번 만들어보자.</h1>
